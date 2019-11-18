deaf1ed8b67fa54680908059d2ec634b4cd03fa9
miguelleonmarti <miguelleonmarti@gmail.com>
2019-11-18 17:01:17 +0000

    Migrations and models (User and Product)

---
+++ app/Product.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+
+class Product extends Model
+{
+    use Notifiable;
+
+    protected $fillable = [''];
+}
